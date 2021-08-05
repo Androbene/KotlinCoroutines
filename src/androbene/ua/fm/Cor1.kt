52216ace3bbcb4660a227f49caf0ee7b9898fc7a
Andrew Druchylo <androbene@ua.fm>
2021-08-05 10:57:40 +0300

    Commit-2 delay added

--- src/androbene/ua/fm/Cor1.kt
+++ src/androbene/ua/fm/Cor1.kt
@@ -3,8 +3,10 @@ package androbene.ua.fm
 import kotlinx.coroutines.*
 
 fun main() = runBlocking {
-    launch (Dispatchers.Default) {
+    val job = launch(Dispatchers.Default) {
         println("Hello from: " + Thread.currentThread().name)
+        delay(1000)
     }
     println("Done!")
+    job.join()
 }
