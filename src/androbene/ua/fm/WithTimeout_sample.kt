dff92fb6b84f5725144af99b55168efa7e63fc7d
Andrew Druchylo <androbene@ua.fm>
2021-08-06 18:04:38 +0300

    added WithTimeout_sample.kt

---
+++ src/androbene/ua/fm/WithTimeout_sample.kt
@@ -0,0 +1,33 @@
+package ua.androbene.kotlin.coroutines
+
+import kotlinx.coroutines.TimeoutCancellationException
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.withTimeout
+
+suspend fun main() = coroutineScope {
+    println(1)
+    val res = withTimeout(2000) {
+        println("inside")
+        delay(1500)
+        100500
+    }
+    println(2)
+    println(res)
+
+    println("------------------------------------------------------------")
+    try {
+        runWithTimeout()
+    } catch (e: TimeoutCancellationException) {
+        println("TimeoutCancellationException: Время вышло, попытка не удалась.")
+    }
+}
+
+suspend fun runWithTimeout() {
+    withTimeout(350) {
+        repeat(5) { n ->
+            delay(100)
+            println("repeat $n")
+        }
+    }
+}
